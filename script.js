fde44ce89542678314232cee1bdd8b50bed55df1
georgekilauridze <99383455+georgekilauridze@users.noreply.github.com>
2022-04-13 12:36:08 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+//  Exercise 1
+
+// function sum(...args) {
+//     let numberSum = 0;
+//     for (let item of args) {
+//         numberSum = numberSum + item
+
+//     }
+
+//     console.log(numberSum);
+// }
+// sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+
+// Exercise 2
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+// }
+
+// Exercise 3
+
+// function getUserName(user) {
+//     if (user.isloggedin = true)
+//         return "giorgi" + " " + "saakadze"
+
+// }
+// let right = getUserName(user);
+// console.log(right);
