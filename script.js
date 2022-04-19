7fdde7e892215ad550a0a2372483c6a792e98e34
georgekilauridze <99383455+georgekilauridze@users.noreply.github.com>
2022-04-19 13:53:14 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-//  Exercise 1
+//lesson2 ex 1 ///
+
+// function sum(...numbers) {
+//        let newSum = 0;
+//        for(let item of numbers) {
+//            if (item > 0) {
+//                newSum = newSum + item;
+//            }
+//        }
+//        console.log(newSum);
+// }
+
+
+// sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6,8);
+
+// function sum(...numbers) {
+//     let newSum = 0;
+//     for(let item of numbers) {
+//         if (item > 0) {
+//             newSum = newSum + item;
+//         }
+//     }
+//    return newSum;
+// }
+
+
+// let result = sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6,8);
+// console.log(result);
+
+
+//  Exercise 2 ///
 
 // function sum(...args) {
 //     let numberSum = 0;
@@ -12,7 +42,7 @@
 // sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
 
 
-// Exercise 2
+//lesson2 ex 3 ///
 
 // let user = {
 //     firstname: 'giorgi',
@@ -21,12 +51,28 @@
 //     isloggedin: true
 // }
 
-// Exercise 3
-
-// function getUserName(user) {
-//     if (user.isloggedin = true)
-//         return "giorgi" + " " + "saakadze"
-
+// function printName(obj) {
+//     if(obj.isloggedin) {
+//         return obj.firstname + ' ' + obj.lastname;
+//     }
+//     return false;
 // }
-// let right = getUserName(user);
-// console.log(right);
+
+// let resultPrintName = printName(user);
+// console.log(resultPrintName);
+
+
+//lesson2 ex 4 ///
+
+// function getMax (numbers) {
+//     let maxValue = 0;
+//     for (let index = 0; index < numbers.length; index ++) {
+//         const x = numbers[index];
+//         if (x > maxValue) {
+//             maxValue = x;
+//         }
+//     }
+//     return maxValue;
+// }
+
+// console.log(getMax([3, 17, 19, 36, 160, 8 ]));
